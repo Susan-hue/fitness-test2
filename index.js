ce060402f83a1e42bacd6fd77e232c29a24297ab
Susan Amechi <amechisusanogechi@gmail.com>
2022-12-03 19:33:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const change1 = document.getElementById("image1");
+
+change1.addEventListener("click", (event) =>{
+  event.target.style.background ="#DFFF00";
+  event.target.style.color ="#6495ED";
+} );
+
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) =>{
+  event.target.style.background ="#008000";
+  event.target.style.color ="#00FFFF";
+} );
+
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) =>{
+  event.target.style.background ="#0000FF";
+  event.target.style.color ="#008080";
+} );
+
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) =>{
+  event.target.style.background ="#540c3b";
+  event.target.style.color ="#844476";
+} );
+
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) =>{
+  event.target.style.background ="#473126";
+  event.target.style.color ="#e6337a";
+} );
+
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) =>{
+  event.target.style.background ="#3a3a3e";
+  event.target.style.color ="#aef950";
+} );
+
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) =>{
+  event.target.style.background ="#2e1c00";
+  event.target.style.color ="#019faf";
+} );
+
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) =>{
+  event.target.style.background ="#6a0f4a";
+  event.target.style.color ="#dcf4da";
+} );
+
+
+const change9 = document.getElementById("image9");
+
+change9.addEventListener("click", (event) =>{
+  event.target.style.background ="#019faf";
+  event.target.style.color ="#f0a7ac";
+} );
+
+
+const change10 = document.getElementById("image10");
+
+change10.addEventListener("click", (event) =>{
+  event.target.style.background ="#543d19";
+  event.target.style.color ="#e3d3d0";
+} );
+
